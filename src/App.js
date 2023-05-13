7351e175df05a2115d9273535cea11dbc4013c78
valerierobles <roblesv08@gmail.com>
2023-05-13 14:27:29 -0500

    put all files in src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Section from "./components/Section";
 import Navbar from "./components/Navbar";
 
---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const styles = {
+  headerStyle: {
+    background: 'blue',
+  },
+  headingStyle: {
+    fontSize: '100px',
+  },
+};
+
+function Header() {
+  return (
+    <header style={styles.headerStyle} className="header">
+      <h1 style={styles.headingStyle}>Valerie Robles</h1>
+    </header>
+  );
+}
+
+export default Header;
