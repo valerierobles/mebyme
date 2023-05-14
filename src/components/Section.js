f26afdbcdfedd5e5e033056a97f692e40bacb906
valerierobles <roblesv08@gmail.com>
2023-05-14 09:58:09 -0500

    styling format from student activity

---
+++ src/components/Section.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/Section.css';
+
+
+
+const styles = {
+  sectionStyles: {
+    background: 'lightblue',
+  },
+};
+
+function Section() {
+  return (
+    <section style={styles.sectionStyles} className="section">
+      <h2>Projects</h2>
+      <p>
+        
+      </p>
+      <p>
+       
+      </p>
+      <p>
+       
+
+      </p>
+    </section>
+  );
+}
+
+export default Section;
