7653ad22775489cffd96673f2b886de332359e5f
valerierobles <roblesv08@gmail.com>
2023-05-13 22:41:21 -0500

    header/navbar porting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "./components/Header";
-import Section from "./components/Section";
 import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+
 
 function App() {
   return (
